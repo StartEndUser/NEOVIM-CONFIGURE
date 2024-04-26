ee8824aa30600997f4d81bb08cd6ade0a8fb5bc6
StartEndUser <StartEndUser@outlook.com>
2024-04-26 21:00:37 +0800

    Removed GDScript Highlight Support Since It Can't Work Properly

--- lua/Plugin/Highlight.lua
+++ lua/Plugin/Highlight.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"bash", "c", "c_sharp", "css", "cpp", "gdscript", "gitignore", "godot_resource", "html", "lua", "make", "nasm", "ninja", "python", "rust",
+  ensure_installed = {"bash", "c", "c_sharp", "css", "cpp" , "gitignore", "godot_resource", "html", "lua", "make", "nasm", "ninja", "python", "rust",
                       "comment", "doxygen"},
+--"gdscript"
 
   highlight = {enable = true},
   indent = {enable = false, disable = true}
